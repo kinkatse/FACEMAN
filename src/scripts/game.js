4018886563ca715533ddc5c532498b8aa572155b
kakintse <kinkatse@mail.adelphi.edu>
2022-06-02 18:37:00 -0400

    Add canvas drawing the face loop

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,76 +1,60 @@
 class Game {
+
     constructor(ctx) {
         this.video = document.getElementById("video");
+        this.ctx = ctx;
+        this.mode = "gameon";
         this.model;
-        // this.model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
 
-        this.createdNewInstance()
-        this.setupCamera(ctx)
-        // this.draw()
+        // Checks to see if game is playing to set up camera and load face models
+        if (this.mode === "gameon"){
+            this.setupCamera();
+            this.loadFaceModel();
+            // Sets interval to loop the draw function (which loses context)
+            setInterval(this.draw.bind(this), 20);
+        }
     }
 
-    createdNewInstance() {
-        console.log('test')
-    }
-
-    setupCamera(ctx) {
-        // this.video = createCapture(VIDEO);
-        // this.video.hide();
-        // loadFaceModel();
+    setupCamera() {
+        // Using the mediaDevices API to grab the video
         navigator.mediaDevices.getUserMedia({
             video: {width: 700, height: 450},
             audio: false,
         }).then( stream => {
+            // Which returns us a promise which we then take the stream and set that to the video srcObject
             this.video.srcObject = stream;
-            // video = document.getElementById("video");
-            // ctx.drawImage(video, 0, 0, 1000, 600);
-            // ctx.drawImage(this.video.srcObject, 0, 0, 700, 450);
         });
-        // this.video = document.getElementById("video");
-        // ctx.drawImage(this.video, 0, 0, 1000, 600);
-        // loadFaceModel();
-
-        // this.video.addEventListener("loadeddata", async () => {
-        //     this.model = await faceLandmarksDetection.load(
-        //         faceLandmarksDetection.SupportedPackages.mediapipeFacemesh
-        //     );
-        //     this.detectFace()
-        // })
-
-        this.loadFaceModel()
     }
 
     loadFaceModel() {
-
+        // Waiting for the video to have a stream from the setupCamera function
         this.video.addEventListener("loadeddata", async () => {
+            // Then loads the face detection API
             this.model = await faceLandmarksDetection.load(
                 faceLandmarksDetection.SupportedPackages.mediapipeFacemesh
             );
-            this.detectFace()
-        })
-
-
-
-        // this.model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
-        // const detectorConfig = {
-        //     runtime: 'mediapipe',
-        //     solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
-        // }
-        // const detector = await faceLandmarksDetection.createDetector(model, detectorConfig);
-        // const faces = await detector.estimateFaces(image);
-        // this.model = await faceLandmarksDetection.load(
-        //     faceLandmarksDetection.SupportedPackages.mediapipeFacemesh
-        // );
+        });
     }
 
-    async detectFace() {
+    async detectFace(ctx) {
+        // Here we grab the model we waited from the API for and grab its preditions
+        // from estimatFaces function passing in the video element in order to detect those faces
         const prediction = await this.model.estimateFaces({
             input: document.querySelector('video')
-        })
+        });
+        this.ctx.drawImage(this.video, 0, 0, 700, 450);
 
-        // debugger
-        // console.log(prediction)
+        console.log(prediction);
     }
+
+    // Load face models predictions before drawing which happens in the detectFace function
+    draw() {
+        // This is so that we don't error out before out video loads up
+        if (this.model !== undefined) {
+            this.detectFace(this.ctx);
+        }
+    }
+
 }
 
 export default Game;
