ab7b23b44019eb098e15172ff9c5b399c8fbb541
kakintse <kinkatse@mail.adelphi.edu>
2022-06-03 11:13:49 -0400

    Add face scan mask behind the dots of facial features

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -94,6 +94,22 @@ class Game {
         // temporarily on for testing
         const faceMaskDots = true
         if (faceMaskDots && this.face !== undefined) {
+            // Making blue mask around face
+            this.ctx.strokeStyle = 'rgba(0, 150, 255, 0.5)';
+            this.ctx.beginPath();
+            let startpt = this.face.annotations.silhouette[0];
+            startpt = this.scaleCoord(startpt);
+            for (let pt of this.face.annotations.silhouette) {
+                pt = this.scaleCoord(pt);
+                this.ctx.lineTo(...pt)
+            }
+            // This is to close the shape
+            this.ctx.lineTo(...startpt);
+            this.ctx.fillStyle = 'rgba(0, 150, 255, 0.3)';
+            this.ctx.fill();
+            this.ctx.stroke();
+            
+            // Making face dots for each facial feature
             for (let pt of this.face.scaledMesh) {
                 this.ctx.beginPath();
                 this.ctx.strokeStyle = "black";
@@ -104,7 +120,7 @@ class Game {
                 this.ctx.fillStyle = "black";
                 this.ctx.fill();
                 this.ctx.stroke();
-              }
+            }
         }
 
         // For Mouth!
