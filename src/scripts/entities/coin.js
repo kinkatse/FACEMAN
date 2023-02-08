ca9684cbfb68b9a3bb9d52a841584345f15ca6ed
kakintse <kinkatse@mail.adelphi.edu>
2023-02-07 19:15:17 -0500

    add coin image in gameplay, and have image animation

--- src/scripts/entities/coin.js
+++ src/scripts/entities/coin.js
@@ -12,6 +12,14 @@ class Coin extends MovingObject {
 
         super(options);
 
+        const frame1 = document.getElementById("coin-icon-1")
+        const frame2 = document.getElementById("coin-icon-2")
+        const frame3 = document.getElementById("coin-icon-3")
+        const frame4 = document.getElementById("coin-icon-4")
+        const frame5 = document.getElementById("coin-icon-5")
+        const frame6 = document.getElementById("coin-icon-6")
+        const frame7 = document.getElementById("coin-icon-7")
+        this.imageArr = [frame1, frame2, frame3, frame4, frame5, frame6, frame7]
         this.score = 50;
     }
 
@@ -24,7 +32,15 @@ class Coin extends MovingObject {
     }
 
     draw(ctx) {
-        MovingObject.prototype.draw.call(this, ctx);
+        // Numbers are to adjust the image correctly
+        ctx.drawImage(this.imageArr[0], this.pos[0] - 26, this.pos[1] - 25, 50, 50)
+        
+        // make the frame go to back of queue
+        const frame = this.imageArr.shift()
+        this.imageArr.push(frame)
+
+
+        // MovingObject.prototype.draw.call(this, ctx);
     }
 }
 
